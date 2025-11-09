eb862c45df36a0b02a67f0ccec08bcc1a68f3bcf
yothunder <tyo.suwignyo@gmail.com>
2025-11-09 22:53:45 +0700

    change url

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -88,7 +88,7 @@ const sendMessage = async () => {
   responseStatus.value = ''
 
   try {
-    const response = await fetch('http://127.0.0.1:8000/api/contact', {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/contact`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(form.value),
